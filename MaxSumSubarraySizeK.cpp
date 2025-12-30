aca37a68f491de8b4074b2cc0776a375eb717217
Raunak Verma <121497156+RAUNAKVERMA22@users.noreply.github.com>
2025-12-30 23:42:38 +0530

    Implement max sum subarray of fixed size K
    This program calculates the maximum sum of a subarray of fixed size K from a given array.

---
+++ MaxSumSubarraySizeK.cpp
@@ -0,0 +1,23 @@
+// Program to find maximum sum subarray of fixed size K
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[] = {2, 1, 5, 1, 3, 2};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int k = 3;
+
+    int sum = 0;
+    for (int i = 0; i < k; i++)
+        sum += arr[i];
+
+    int maxSum = sum;
+
+    for (int i = k; i < n; i++) {
+        sum += arr[i] - arr[i - k];
+        maxSum = max(maxSum, sum);
+    }
+
+    cout << "Maximum sum subarray of size " << k << " = " << maxSum;
+    return 0;
+}
